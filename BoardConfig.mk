b7b0d51d8744276b58beb271fc6f675701373e9d
chandu078 <chandudyavanapelli03@gmail.com>
2026-02-07 05:13:49 +0000

    infiniti: Switch to prebuilt kernel
    Change-Id: I7cc8459071da010f873fd49122dcab020d9eb4ba

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+USE_PREBUILT_KERNEL ?= true
+
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 17062428672
 
@@ -19,7 +21,11 @@ TARGET_OTA_ASSERT_DEVICE := OP611FL1,OP60FFL1
 TARGET_SCREEN_DENSITY := 560
 
 # Kernel
+ifeq ($(USE_PREBUILT_KERNEL), true)
+include device/oneplus/infiniti-kernel/BoardConfig.mk
+else
 TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_INFINITI_DTB=y
+endif
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
